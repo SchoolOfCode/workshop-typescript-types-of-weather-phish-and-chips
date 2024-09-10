74871ef4cefa9e8ec812d4bcbb983920e79848f9
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-09-10 14:13:25 +0100

    added sign in logout logic

---
+++ my-app/src/App.js
@@ -0,0 +1,43 @@
+import "./index.css";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const handleLogout = async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) console.log("Error logging out:", error.message);
+};
+
+export default function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return (
+      <>
+        <div>Logged in!</div>
+        <button onClick={handleLogout}>Logout</button>
+      </>
+    );
+  }
+}
